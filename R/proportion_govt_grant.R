de825551d865d8ca7c2edd9b9e01df1e9a6a9367
Thiyaghessan <thiyaghessan@gmail.com>
2025-02-06 22:40:21 -0500

    Update formaula and add documentation

--- R/proportion_govt_grant.R
+++ R/proportion_govt_grant.R
@@ -1,13 +1,23 @@
+#' @title This function computes the proportion of revenue derived from government grants
+#' @description Government grants from Part 08 Line 1e of the 2024 Form 990.
+#' Total revenue from Part 08 Line 12 of the 2024 Form 990. With additional
+#' handling of edge cases.
+#' @param govt_grant numeric scalar of government grants
+#' @param total_revenue numeric scalar of total revenue
+#' @return numeric scalar of the proportion of revenue derived from government grants
 proportion_govt_grant <- function(govt_grant, total_revenue) {
+  if (all(is.na(c(govt_grant, total_revenue)))) return(NA_real_)
+  else if (is.na(govt_grant) && !is.na(total_revenue)) return(0)
+  else if (is.na(total_revenue) && !is.na(govt_grant)) return(NA_real_)
   
-  if (is.na(total_revenue)){
-    proportion <- NA
-  } else if (is.na(govt_grant)){
-    proportion <- 0
-  } else if (total_revenue != 0 && govt_grant != 0) {
-    proportion <- govt_grant / total_revenue
+  
+  proportion <- if (total_revenue != 0 && govt_grant != 0) {
+    govt_grant / total_revenue
   } else {
-    proportion <- 0
+    0
   }
-  return(proportion)
+  
+  if (proportion < 0 || proportion > 1) return(NA_real_)
+  
+  proportion
 }
