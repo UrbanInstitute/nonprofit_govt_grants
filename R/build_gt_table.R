98f46cd374ca2c9c789779304a37802a8d346d7f
Thiyaghessan <thiyaghessan@gmail.com>
2025-02-07 14:20:13 -0500

    Debug incorrect function name

--- R/build_gt_table.R
+++ R/build_gt_table.R
@@ -1,15 +1,16 @@
 #' @title This function creates a table with the GT package for visualizing
 #' a table for the factsheet
 
-create_gt_table <- function(df_path, groupby_var, groupby_name) {
+build_gt_table <- function(df_path, groupby_var, groupby_name) {
   gt_data <- readr::read_csv(df_path)
+  gt_data <- gt_data |>
+    dplyr::select(!mean_months_cash_on_hand_tangibleassets) |>
+    dplyr::select(!mean_months_cash_on_hand_notangibleassets)
   gt_table <- gt_data %>%
     gt() %>%
     cols_label(
       "{groupby_var}" := groupby_name,
-      mean_months_cash_on_hand_tangibleassets = "Mean",
       median_months_cash_on_hand_tangibleassets = "Median",
-      mean_months_cash_on_hand_notangibleassets = "Mean",
       median_months_cash_on_hand_notangibleassets = "Median",
       total_govt_grants = "Total (USD)",
       proportion_govt_grants_20 = "0-20%",
@@ -21,13 +22,11 @@ create_gt_table <- function(df_path, groupby_var, groupby_name) {
     ) %>%
     tab_spanner(
       label = html("Months of cash on hand<br>(with tangible assets)"),
-      columns = c(mean_months_cash_on_hand_tangibleassets, 
-                  median_months_cash_on_hand_tangibleassets)
+      columns = c(median_months_cash_on_hand_tangibleassets)
     ) %>%
     tab_spanner(
       label = html("Months of cash on hand<br>(without tangible assets)"),
-      columns = c(mean_months_cash_on_hand_notangibleassets,
-                  median_months_cash_on_hand_notangibleassets)
+      columns = c(median_months_cash_on_hand_notangibleassets)
     ) %>%
     tab_spanner(
       label = html("Dollar value of funding<br>from government grants"),
@@ -55,22 +54,7 @@ create_gt_table <- function(df_path, groupby_var, groupby_name) {
     ) %>%   tab_style(
       style = cell_text(align = "center"),
       locations = cells_body()
-    ) %>%
-    tab_style(
-      style = cell_borders(sides = "left", color = "black", weight = px(1)),
-      locations = cells_body()
-    ) %>%
-    tab_style(
-      style = list(
-        cell_borders(sides = c("left", "right"), color = "black", weight = px(1))),
-      locations =list(
-        cells_body(),
-        cells_column_labels(),
-        cells_column_spanners()
-      )
     ) |>
-    tab_source_note(source_note = html("<b>Source</b>: National Center for Charitable Statistics 2021 501(c)(3) Charities PZ Scope, available at https://urbaninstitute.github.io/nccs/catalogs/catalog-core.html.")) |>
-    opt_horizontal_padding(scale = 3) |>
-    opt_vertical_padding(scale = 3)
+    tab_options(data_row.padding.horizontal = px(20))
   return(gt_table)
 }
