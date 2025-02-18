59b3a553c19be2bc5fff41f0f1c59f8521870907
Thiyaghessan <thiyaghessan@gmail.com>
2025-02-17 20:41:05 -0500

    Script for internal objects

---
+++ R/data.R
@@ -0,0 +1,11 @@
+# List of efile urls
+efile_urls <- list(
+  "efile_p01_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P01-T00-SUMMARY-2021.csv",
+  "efile_p08_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P08-T00-REVENUE-2021.csv",
+  "efile_p09_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P09-T00-EXPENSES-2021.csv",
+  "efile_p10_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P10-T00-BALANCE-SHEET-2021.csv"
+)
+
+bmf_urls <- list(
+  "unified_bmf.csv" = "https://nccsdata.s3.amazonaws.com/harmonized/bmf/unified/BMF_UNIFIED_V1.1.csv"
+)
