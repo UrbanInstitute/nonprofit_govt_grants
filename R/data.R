546cfe14d5a95a5408abfcf11bd4dbfc845ab321
Thiyaghessan <thiyaghessan@gmail.com>
2025-02-18 18:02:39 -0500

    Add download link for efile header

--- R/data.R
+++ R/data.R
@@ -1,5 +1,6 @@
 # List of efile urls
 efile_urls <- list(
+  "efile_hd_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P00-T00-HEADER-2021.csv",
   "efile_p01_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P01-T00-SUMMARY-2021.csv",
   "efile_p08_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P08-T00-REVENUE-2021.csv",
   "efile_p09_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P09-T00-EXPENSES-2021.csv",
