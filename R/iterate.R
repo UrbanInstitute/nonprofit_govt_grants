4cad44963dc70264f07e41a198466e7069cbab74
Thiyaghessan <thiyaghessan@gmail.com>
2025-02-19 16:20:29 -0500

    Remove redundant code

--- R/iterate.R
+++ R/iterate.R
@@ -26,16 +26,3 @@ for (state in states) {
     params = list(state = state)
   )
 }
-
-#make a list of all staging links
-all_links <- data.frame(msa_identifiers$directory_name) %>% 
-  rename(dir = msa_identifiers.directory_name) %>% 
-  mutate(link = paste0("https://apps-staging.urban.org/features/spcp-msas-r-markdown/factsheets/",dir,".html"))
-
-write_csv(all_links, "all_staging_links.csv")
-
-dropdown_text <- msa_identifiers %>% 
-  mutate(text = paste0("<option value='",directory_name,"'><a href='https://datacatalog.urban.org/dataset/'>",city_state,"</a></option>")) %>% 
-  select(text)
-
-write_csv(dropdown_text, "dropdown_text_data_cat.csv")
