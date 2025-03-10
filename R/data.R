3f02bb2ed266d7ea113db2119438ba80345655df
Thiyaghessan <thiyaghessan@gmail.com>
2025-03-10 13:27:42 -0400

    Add URL for part V

--- R/data.R
+++ R/data.R
@@ -2,6 +2,7 @@
 efile_urls <- list(
   "efile_hd_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P00-T00-HEADER-2021.csv",
   "efile_p01_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P01-T00-SUMMARY-2021.csv",
+  "efile_p05_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P05-T00-PROGRAM-2021.csv",
   "efile_p08_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P08-T00-REVENUE-2021.csv",
   "efile_p09_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P09-T00-EXPENSES-2021.csv",
   "efile_p10_2021_0225.csv" = "https://nccs-efile.s3.us-east-1.amazonaws.com/public/v2025/F9-P10-T00-BALANCE-SHEET-2021.csv"
